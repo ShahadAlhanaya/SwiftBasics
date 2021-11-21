e70b824ce5c835c86cf2944ed6fccd22a2d367ee
ShahadNasser <Shahad.N.Alhanaya@gmail.com>
2021-11-21 16:01:29 +0300

    terminal add

--- SwiftBasics.playground/Pages/Fundementals_1.xcplaygroundpage/Contents.swift
+++ SwiftBasics.playground/Pages/Fundementals_1.xcplaygroundpage/Contents.swift
@@ -1,7 +1,10 @@
 import UIKit
 
+//changed to constants
 let type: String = "Rectangle"
 let description: String = "A quadrilateral with four right angles"
+
+//specify type as double to keep calculation percise
 var width: Double = 5
 var height: Double = 10.5
 var area = width * height
@@ -9,3 +12,4 @@ height += 1
 width += 1
 area = width * height
 print("The shape is a \(type) or \(description) with an area of \(area)")
+
